926cbfd4c59e562e9568498686aa3befbdc484e7
Nicholas Marchese <NikM@NikMs-MacBook-Pro.local>
2016-12-16 15:50:52 -0700

    CardPlayer complete, ready to start driver

--- src/cardGame/CardPlayer.java
+++ src/cardGame/CardPlayer.java
@@ -1,5 +1,19 @@
 package cardGame;
 
 public class CardPlayer {
-
+	private Hand hand;
+	private String name;
+	
+	public CardPlayer(String name) {
+		this.name = name;
+		hand = new Hand();
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public Hand getHand() {
+		return hand;
+	}
 }
--- src/cardGame/Hand.java
+++ src/cardGame/Hand.java
@@ -27,4 +27,8 @@ public class Hand {
 		}
 		System.out.println("Total Value: " + getHandValue());
 	}
+	
+	public void resetHand() {
+		hand = new LinkedList<>();
+	}
 }
